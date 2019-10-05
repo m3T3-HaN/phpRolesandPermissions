0d5807b77d556eaab4d94a6178873ce7f11e33d1
Metehan BOY <m3T3-HaN@hotmail.com>
2019-10-05 17:51:20 +0300

    Create searchFunction.php
    Bu sistem mvc modülü yada class import yapısıyla oluşturduğunda perm fonksiyonuna her seferinde izinleri yollamaktansa veritabanından ilgili kullanıcı için ilgili izinleri alıp direk uygulayabilirsiniz.

---
+++ searchFunction.php
@@ -0,0 +1,31 @@
+<?php
+
+    $perms = array(
+      'auth.login',
+      'auth.register',
+      'dashboard.open',
+      'dashboard.message.send',
+      'dashboard.message.read'
+    );
+
+    function perm($check,$perms){
+
+        $searchEx = explode('.',$check);
+        
+        $perms = array_map(function($perm){return explode('.',$perm);},$perms);
+
+        $tmp = [];
+        foreach ($perms as $perm){
+            if(sizeof($searchEx) == 1 && $searchEx[0] == '*'){
+                return true;
+            }
+            foreach ($searchEx as $key => $req){
+                $tmp[$key] = $req == '*' ? 1 : ($req == $perm[$key] ? 1 : 0);
+            }
+            if(sizeof($perm) == array_sum($tmp)) return true;
+        }
+        return sizeof($perm) == array_sum($tmp);
+    }
+    
+    echo perm('auth.recoverpassword',$perms) ? 'izin var' : 'izin yok'; //Çıktısı: izin yok
+    echo perm('auth.*',$perms) ? 'izin var' : 'izin yok'; //Çıktısı: izin var
